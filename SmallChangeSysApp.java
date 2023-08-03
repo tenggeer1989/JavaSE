b9d1e98aebd6a34a28551732154abce083490e0c
TENGGEER\tengg <tenggeer1989@outlook.com>
2023-08-03 19:09:32 +0800

    2023/8/3 零钱通（面向对象小案例）

---
+++ SmallChangeSysApp.java
@@ -0,0 +1,16 @@
+package com.tge.test.oop;
+
+/**
+ * ClassName: SmallChangeSysApp
+ * Package: com.tge.test.oop
+ * Description:
+ *
+ * @Author: tge
+ * @Create: 2023/8/1 - 21:46
+ * Version:
+ */
+public class SmallChangeSysApp {
+    public static void main(String[] args) {
+         new SmallChangeSysOOP().mainMenu();
+    }
+}
