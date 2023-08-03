02603087e92e3bbbfe3669f76d5f67d19317f32b
TENGGEER\tengg <tenggeer1989@outlook.com>
2023-08-03 19:11:00 +0800

    2023/8/3 房屋出租系统（面向对象小案例）

---
+++ HouseRentAPP.java
@@ -0,0 +1,17 @@
+package com.tge.houserent;
+
+/**
+ * ClassName: HouseRentAPP
+ * Package: com.tge.houserent
+ * Description:
+ *
+ * @Author: tge
+ * @Create: 2023/8/2 - 21:13
+ * Version:
+ */
+public class HouseRentAPP {
+    public static void main(String[] args) {
+        new HouseView().mainMenu();
+        System.out.println("===您已退出房屋出租系统，欢迎下次再使用！！！===");
+    }
+}
